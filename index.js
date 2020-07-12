1ba13a0bf252af25805a17fdb49b94e980fc13e8
oscarzamora <oscarnewsletter1@gmail.com>
2020-07-12 16:25:56 -0500

    a punto de usar los metodos http

--- index.js
+++ index.js
@@ -13,15 +13,23 @@ server.listen(3000, () => {
 const express = require("express");
 const app = express();
 
-//creando ruta about
+//creando ruta about con un h1 dentro
 app.get("/about", (req, res) => {
   res.send("<h1>about me</h1>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Formulario de contacto");
+});
+
+app.get("/test", (req, res) => {
+  res.send("<h1>test</h1>");
 });
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.listen(3000, () => {
-  console.log("Servidor en el puerto 3000");
+app.listen(5000, () => {
+  console.log("Servidor en el puerto 5000");
 });
